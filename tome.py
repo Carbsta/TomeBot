e279cc2520a281a433784e2ec131c9ff61b8179c
Unknown <totallycarbon@gmail.com>
2017-07-26 23:33:46 +0100

    Version 2
    Rewrite of the code to use async better, to help avoid crashes. No new features, although a reworked roll command is coming soon and magic items is on the horizon.
    
    Version 3 is going to be fully database based and is coming in the not too distant future.

--- tome.py
+++ tome.py
@@ -1,13 +1,15 @@
 import asyncio
 import discord
+import aiohttp
 import json
-import random
-import operator
 import re
+import random
+import sys
+import os
+import operator
 
 #operator look up table for the diceroller
 ops = {"+":operator.add,"-":operator.sub}
-
 #edit these values if you want to load the various json files from different folders.
 paths = {"license":"license.json","spells":"spells.json","monsters":"monsters.json","token":"token.json","log":"log.json"}
 
@@ -37,56 +39,58 @@ class TomeBot(discord.Client):
     def __init__(self):
         super().__init__()
         self.GamePlaying = discord.Game()
-        self.GamePlaying.name = "Type ?commands"
-
-    async def on_message(self, message):
-        global log
-        if message.content.startswith("?"):
-            if len(log) > 9:
-                log = log[-9:]
-            try:
-                messagelog = {"messageID":message.id,"userID":message.author.id,"username":message.author.name,"timestamp":str(message.timestamp),"content":message.content,"serverID":message.server.id,"servername":message.server.name,"channelID":message.channel.id,"channelname":message.channel.name,"privatemessage":False}
-            except:
-                messagelog = {"messageID":message.id,"userID":message.author.id,"username":message.author.name,"timestamp":str(message.timestamp),"content":message.content,"privatemessage":True}
-            log.append(messagelog)
-            with open(paths["log"],'w') as fp:
-                json.dump(log, fp)
-            command = (message.content.split(' ',1)[0])[1:]
-            if hasattr(self, command):
-                response = getattr(self, command)(message)
-                if type(response) is list:
-                    if response[0] != None:
-                        for a in response:
-                            await self.send_message(message.channel, a)
-                else:
-                    await self.send_message(message.channel,embed=response)
-
+        self.GamePlaying.name = "VERSION 2! Hopefully doesn't crash anymore!"
+        self.ownid = tokens["ownid"]
+        self.ownerid = tokens["ownerid"]
+        self.commandset = {"commands","info","spellsearch","spellinfo","monstersearch","monsterinfo","roll"}
+        self.ownercommands = {"restart"}
+        self.isowner = False
 
     async def on_ready(self):
-        print('Logged in as')
-        print(self.user.name)
-        print(self.user.id)
-        print('------')
-        await self.change_presence(game=self.GamePlaying)
+        await self.change_presence(game = self.GamePlaying)
 
-    def commands(self, message):
+    async def fetchcommand(self,_message):
+        if _message.author.id == self.ownid:
+            return("False")
+        if _message.content.startswith("?") == False:
+            return("False")
+        return((_message.content.split(' ',1)[0])[1:])
+
+    async def on_message(self, message):
+        command = await self.fetchcommand(message)
+        if command in self.commandset:
+            method = getattr(self, command)
+            response = await method(message)
+            if isinstance(response, discord.embeds.Embed):
+                await self.send_message(message.channel, embed=response)
+            elif isinstance(response, list):
+                for x in response:
+                    await self.send_message(message.channel, x)
+            else:
+                await self.send_message(message.channel, response)
+        elif (command in self.ownercommands) and (message.author.id == self.ownerid):
+            method = getattr(self, command)
+            await method()
+
+
+    async def commands(self, message):
         if message.content.lower().endswith("-p"):
             response= """
 Commands:
-?roll - roll dice, syntax ?roll xdy
+?roll - roll dice, syntax ?roll xdy (improved ?roll coming soon :tm:)
 ?spellsearch - search for a dnd 5e spell
 ?spellinfo - get information about a specific dnd 5e spell
 ?monstersearch - search for a dnd 5e monster
 ?monsterinfo - get information about a specific dnd 5e monster
 suffixes: -p (plain) forces responses to be in plaintext without embeds.
 """
-            return([response])
+            return(response)
         else:
             response = discord.Embed()
             response.colour = discord.Colour.dark_purple()
             response.title = "Bot Commands"
             response.type = "rich"
-            response.add_field(name="?roll",value="Rolls dice. Can add or subtract modifiers to individual dice or the total of the rolls.\n \"?roll xdy+z\" - adds modifier to each dice.\n \"?roll xdy +z\" - adds modifier to the total.",inline=False)
+            response.add_field(name="?roll (IMPROVED ?ROLL COMING SOON :tm:)",value="Rolls dice. Can add or subtract modifiers to individual dice or the total of the rolls.\n \"?roll xdy+z\" - adds modifier to each dice.\n \"?roll xdy +z\" - adds modifier to the total.",inline=False)
             response.add_field(name="?spellsearch",value="Searches for a 5e spell.\nNot case sensitive, and uses metadata to help you search.\nYou can search by class, level, name, components, range and more.\nSeperate search terms with commas.\n \"?spellsearch 1st, Bard, touch\" - searches for 1st level Bard spells with a range of touch.",inline=False)
             response.add_field(name="?spellinfo",value="Gives information about a spell.\nNot case sensitive, but does have to be spelt correctly.\n \"?spellinfo Cure Wounds\"",inline=False)
             response.add_field(name="?monstersearch",value="Same as spellsearch, but for 5e monsters\nAlso works with metadata such as size, challenge rating, etc.",inline=False)
@@ -95,74 +99,68 @@ suffixes: -p (plain) forces responses to be in plaintext without embeds.
             response.add_field(name="suffixes",value="Use -p (stands for plain) to use a command without it returing an embed.\nFor those who have link embeds turned off in their discord settings.\n \"?commands -p\" displays this page in a non embed format.",inline=False)
             return(response)
 
-    def info(self, message):
+    async def info(self, message):
         inguilds = 0
         for sever in self.servers:
             inguilds = inguilds + 1
         response = """
+**NOW ON VERSION 2!!! 100% MORE STABLE**
+(Finally)
 Type ?commands to see the commands.
 Add the suffix -p to disable embeds.
-?commands -p
+e.g: ?commands -p
 
 Find the owner of the Bot here:
-https://discord.gg/25bf5NT
+<https://discord.gg/25bf5NT>
 
 also on github:
-https://github.com/Carbsta/TomeBot
+<https://github.com/Carbsta/TomeBot>
 
 Hosted by @Crablabuk.
 
 To add to your server use this link:
-https://discordapp.com/oauth2/authorize?client_id=247413966094073856&scope=bot&permissions=0
+<https://discordapp.com/oauth2/authorize?client_id=247413966094073856&scope=bot&permissions=0>
 """
         response = response +"\n\nCurrently in "+str(inguilds)+" discord server(s)."
-        return([response])
+        return(response)
 
+    async def restart(self):
+        print("restarting")
+        os.execv(sys.executable, ['python3'] + sys.argv)
+        return("restarted!")
 
+    async def spellsearch(self, message):
+        words = message.content
+        if words.endswith("-p"):
+            words = words[:-2]
+        searchterms = words.split(' ',1)[1].lower()
+        searchterms = searchterms.split(", ")
+        results = "Results: \n"
+        for spell in spells:
+            matches = 0
+            for term in searchterms:
+                if term in spell['name'].lower():
+                    matches = matches + 1
+                elif term in spell['class'].lower():
+                    matches = matches + 1
+                elif term in spell['school'].lower():
+                    matches = matches + 1
+                elif term in spell['level'].lower():
+                    matches = matches + 1
+                elif term in spell['duration'].lower():
+                    matches = matches + 1
+                elif term in spell['range'].lower():
+                    matches = matches + 1
+                elif ", M" in spell["components"]:
+                    if term in spell["material"]:
+                        matches = matches + 1
+            if matches == len(searchterms):
+                results = results+spell['name']+"\n"
+        if len(results)>1990:
+            results = "Too many results found, try narrowing your search with more search terms."
+        return(results)
 
-    def roll(self, message):
-        try:
-            words = message.content.split(" ")
-            dicenumbers = words[1].split("d")
-            try:
-                modifierint = words[2][1:]
-                modifiersign = words[2][:1]
-            except:
-                modifierint = ""
-                modifiersign = ""
-            total = 0
-            rolls = "Rolls are: "
-            if len(dicenumbers[0]) < 4:
-                for x in (range(0,int(dicenumbers[0]))):
-                    if "+" in dicenumbers[1]:
-                        values = dicenumbers[1].split("+")
-                        roll = random.randint(1,int(values[0]))
-                        rolls += str(roll)+"+"+values[1]
-                        roll = roll + int(values[1])
-                        rolls += "="+str(roll)+", "
-                    elif "-" in dicenumbers[1]:
-                        values = dicenumbers[1].split("-")
-                        roll = random.randint(1,int(values[0]))
-                        rolls += str(roll)+"-"+values[1]
-                        roll = roll - int(values[1])
-                        rolls += "="+str(roll)+", "
-                    else:
-                        roll = random.randint(1,int(dicenumbers[1]))
-                        rolls += str(roll)+", "
-                    total += roll
-                if modifiersign == "":
-                    rolls = rolls[:-2]+". Total = "+str(total)
-                else:
-                    rolls = rolls[:-2]+". "+modifiersign+" "+modifierint+". Total = "+str(ops[modifiersign](total,int(modifierint)))
-                if len(rolls)>1900:
-                    rolls = "Don't do stupid stuff with the Roll command."
-            else:
-                rolls = "Don't do stupid stuff with the Roll command."
-        except:
-            rolls = "Don't do stupid stuff with the Roll command."
-        return([rolls])
-
-    def spellinfo(self, message):
+    async def spellinfo(self, message):
         noembed = False
         words = message.content
         if message.content.endswith("-p"):
@@ -236,38 +234,7 @@ https://discordapp.com/oauth2/authorize?client_id=247413966094073856&scope=bot&p
         else:
             return(results)
 
-    def spellsearch(self, message):
-        words = message.content
-        if words.endswith("-p"):
-            words = words[:-2]
-        searchterms = words.split(' ',1)[1].lower()
-        searchterms = searchterms.split(", ")
-        results = "Results: \n"
-        for spell in spells:
-            matches = 0
-            for term in searchterms:
-                if term in spell['name'].lower():
-                    matches = matches + 1
-                elif term in spell['class'].lower():
-                    matches = matches + 1
-                elif term in spell['school'].lower():
-                    matches = matches + 1
-                elif term in spell['level'].lower():
-                    matches = matches + 1
-                elif term in spell['duration'].lower():
-                    matches = matches + 1
-                elif term in spell['range'].lower():
-                    matches = matches + 1
-                elif ", M" in spell["components"]:
-                    if term in spell["material"]:
-                        matches = matches + 1
-            if matches == len(searchterms):
-                results = results+spell['name']+"\n"
-        if len(results)>1990:
-            results = "Too many results found, try narrowing your search with more search terms."
-        return([results])
-
-    def monstersearch(self,message):
+    async def monstersearch(self, message):
         searchterm = message.content.split(' ',1)[1].lower()
         searchterm = searchterm.split(", ")
         results = "Results: \n"
@@ -292,55 +259,9 @@ https://discordapp.com/oauth2/authorize?client_id=247413966094073856&scope=bot&p
                 results = results+monster['name']+"\n"
         if len(results)>1990:
             results = "Too many results found, try narrowing your search with more search terms."
-        return([results])
-        
-    def deprecated(self,message):
-        searchterm = message.content.split(' ',1)[1].lower()
-        result = "Could not find that monster, use ?monstersearch to get monster names."
-        abilities = "No special abilities"
-        actions = "No actions"
-        legendaryactions = "No legendary actions"
-        for x in monsters:
-            if searchterm == x['name'].lower():
-                result = "**"+x['name']+"**\n\n"+"Size: "+x['size']+"\nChallenge Rating: "+x['challenge_rating']+"\nType: "+x['type']
-                if x['subtype'] != "":
-                    result = result + "\nSubtype: "+x['subtype']
-                result = result +"\nAlignment: "+x['alignment']+"\nSenses: "+x['senses']+"\nLanguages: "+x['languages']
-                try:
-                    abilities = "**Special abilities:**\n\n"
-                    for y in x['special_abilities']:
-                        abilities = abilities + y['name']+"\n"+y['desc']+"\n\n"
-                except:
-                    abilities = "No special abilities"
-                try:
-                    actions = "**Actions:**\n\n"
-                    for z in x['actions']:
-                        actions = actions + z['name']+"\n"+z['desc']+"\n\n"
-                except:
-                    actions = "No actions"
-                try:
-                    legendaryactions = "**Legendary Actions:**\n\n"
-                    for w in x['legendary_actions']:
-                        legendaryactions = legendaryactions + w['name']+"\n"+w['desc']+"\n\n"
-                except:
-                    legendaryactions = "No legendary actions"
-                result = result + "\n\n**For DM specific info such as Stats, armor class etc, use ?dminfo instead**"
-        results = [result,abilities,actions,legendaryactions]
-        for b in results:
-            if len(b)>1900:
-                firstpart, secondpart = b[:len(b)//2], b[len(b)//2:]
-                index = results.index(b)
-                results.remove(b)
-                results.insert(index,secondpart)
-                results.insert(index,firstpart)
-
         return(results)
 
-    def dminfo(self,message):
-        response = "dminfo is now deprecated, ?monsterinfo now displays everything dminfo used to display."
-        return([response])
-
-    def monsterinfo(self,message):
+    async def monsterinfo(self, message):
         noembed = False
         searchterm = message.content.split(' ',1)[1].lower()
         if message.content.endswith("-p"):
@@ -497,6 +418,48 @@ https://discordapp.com/oauth2/authorize?client_id=247413966094073856&scope=bot&p
                 results.insert(index,secondpart)
                 results.insert(index,firstpart)
         return(results)
+
+    async def roll(self, message):
+        try:
+            words = message.content.split(" ")
+            dicenumbers = words[1].split("d")
+            try:
+                modifierint = words[2][1:]
+                modifiersign = words[2][:1]
+            except:
+                modifierint = ""
+                modifiersign = ""
+            total = 0
+            rolls = "Rolls are: "
+            if len(dicenumbers[0]) < 4:
+                for x in (range(0,int(dicenumbers[0]))):
+                    if "+" in dicenumbers[1]:
+                        values = dicenumbers[1].split("+")
+                        roll = random.randint(1,int(values[0]))
+                        rolls += str(roll)+"+"+values[1]
+                        roll = roll + int(values[1])
+                        rolls += "="+str(roll)+", "
+                    elif "-" in dicenumbers[1]:
+                        values = dicenumbers[1].split("-")
+                        roll = random.randint(1,int(values[0]))
+                        rolls += str(roll)+"-"+values[1]
+                        roll = roll - int(values[1])
+                        rolls += "="+str(roll)+", "
+                    else:
+                        roll = random.randint(1,int(dicenumbers[1]))
+                        rolls += str(roll)+", "
+                    total += roll
+                if modifiersign == "":
+                    rolls = rolls[:-2]+". Total = "+str(total)
+                else:
+                    rolls = rolls[:-2]+". "+modifiersign+" "+modifierint+". Total = "+str(ops[modifiersign](total,int(modifierint)))
+                if len(rolls)>1900:
+                    rolls = "Don't do stupid stuff with the Roll command."
+            else:
+                rolls = "Don't do stupid stuff with the Roll command."
+        except:
+            rolls = "Don't do stupid stuff with the Roll command."
+        return(rolls)
 
 bot = TomeBot()
 bot.run(token)
